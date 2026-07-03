4232b021f944b8844a956bbd69f65b2a46bf5cac
Muhammad Anas <daggaanas0888@gmail.com>
2026-07-03 16:08:25 +0530

    make X and O turn for player

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const playerBtns = document.querySelectorAll('.playBtn');
 
+
+
+let turn = true;
+
+
+
+
 playerBtns.forEach ((playBtn) => {
 playBtn.addEventListener('click' , () => {
     console.log("button was clicked ")
-    playBtn.innerText = "X"
+    
+
+if(turn === true){
+playBtn.innerText = "O";
+turn = false;
+    }
+    else{
+        playBtn.innerText = "X";
+        turn = true;
+}
     }) 
     })
 
